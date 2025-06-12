731fd753ade9d432f9325086fc42a102e72baead
Nitesh Choudhary <niteshc69@gmail.com>
2025-06-12 19:59:21 +0530

    get flight price list code update with map collection

--- src/test/java/Selenium_Concept/Selenium_Concept/Flight_Select_page.java
+++ src/test/java/Selenium_Concept/Selenium_Concept/Flight_Select_page.java
@@ -2,9 +2,12 @@ package Selenium_Concept.Selenium_Concept;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.TreeMap;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -13,33 +16,34 @@ import org.testng.annotations.Test;
 
 public class Flight_Select_page extends Flilght_booking_site {
 
-	
-	
-	@Test
+	@Test(priority = 6)
 	public void ss() throws InterruptedException {
-		FluentWait<WebDriver> wait = new FluentWait<>(driver)
-			    .withTimeout(Duration.ofSeconds(15))
-			    .pollingEvery(Duration.ofSeconds(5))
-			    .ignoring(NoSuchElementException.class);	
+		FluentWait<WebDriver> wait = new FluentWait<>(driver).withTimeout(Duration.ofSeconds(15))
+				.pollingEvery(Duration.ofSeconds(5)).ignoring(NoSuchElementException.class);
 
-		
-		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@class=\"splitClsListingDetailsBottom\"]")));
+		wait.until(
+				ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@class=\"splitClsListingDetailsBottom\"]")));
 		Thread.sleep(10000);
-		
-	List<WebElement> images = driver.findElements(By.xpath("//*[@class=\"splitClsListingDetailsBottom\"]"));
-	
-	
-	for (WebElement listimg :images) {
-		System.out.println(listimg);
+
+		List<WebElement> priceElements = driver.findElements(By.xpath("//*[@class='fontSize22 boldFont' and text()]"));
+		Map<Integer, WebElement> priceMap = new TreeMap<>(); // TreeMap keeps keys sorted
+
+		for (WebElement element : priceElements) {
+			String text = element.getText().replaceAll("[^0-9]", "");
+			if (!text.isEmpty()) {
+				int priceValue = Integer.parseInt(text);
+				priceMap.put(priceValue, element);
+			}
+		}
+
+		// Click the lowest-priced element
+		WebElement cheapestOption = priceMap.entrySet().iterator().next().getValue();
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].click();", cheapestOption);
+		driver.manage().window().maximize();
+
+		js.executeScript("arguments[0].scrollIntoView(true);", cheapestOption);
+
 	}
-	
-	
-	
-	
-	
-	}
-	
-	
-	
-	
+
 }
--- src/test/java/Utilities/Browser.java
+++ src/test/java/Utilities/Browser.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
 
 public class Browser  {
 	public   WebDriver driver;
